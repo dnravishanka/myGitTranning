5b119ac404f12cec258b2b4fecd46c3c9a7886c2
nadun <ravishankanadun6@gmail.com>
2023-04-28 19:12:40 +0530

    second committing

--- src/main/java/lk/home/Main.java
+++ src/main/java/lk/home/Main.java
@@ -2,6 +2,25 @@ package lk.home;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
+
     }
+
+    public void m(String ms) {
+
+    }
+
+    public void mi() {
+
+    }
+    
 }
